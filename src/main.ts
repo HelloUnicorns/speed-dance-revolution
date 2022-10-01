c5b4c6ac261264b67d5eade39594efb3b12af38b
ClassyLobster <55981867+ClassyLobster@users.noreply.github.com>
2022-10-01 11:09:22 +0300

    Add arrows sprite.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { Application, Sprite } from 'pixi.js';
 
 const app = new Application({ width: 800, height: 600 });
 document.body.appendChild(app.view);
-const sprite = Sprite.from('images/phaser3-logo.png');
+const sprite = Sprite.from('images/arrows.png');
 app.stage.addChild(sprite);
