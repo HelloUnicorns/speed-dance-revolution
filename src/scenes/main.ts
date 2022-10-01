9020a8e84461e7b7d51b3922f0c70489a211bc21
mrnomm <55981867+mrnomm@users.noreply.github.com>
2022-10-01 19:20:41 +0300

    Add arrow keys press events that print to console.

--- src/scenes/main.ts
+++ src/scenes/main.ts
@@ -1,17 +1,19 @@
 import { Container, PI_2, Sprite } from 'pixi.js';
+import { keyboard } from '../utils/keyboard';
 
 interface Direction {
   order: number;
   name: string;
   rotation: number;
   color: number;
+  key: string;
 }
 
 const DIRECTIONS: Direction[] = [
-  { order: 0, name: 'left', rotation: 0, color: 0xff3300 },
-  { order: 1, name: 'down', rotation: 0.75 * PI_2, color: 0xffff00 },
-  { order: 2, name: 'up', rotation: 0.25 * PI_2, color: 0x00ff00 },
-  { order: 3, name: 'right', rotation: 0.5 * PI_2, color: 0x3377ff },
+  { order: 0, name: 'left', rotation: 0, color: 0xff3300, key: 'ArrowLeft' },
+  { order: 1, name: 'down', rotation: 0.75 * PI_2, color: 0xffff00, key: 'ArrowDown' },
+  { order: 2, name: 'up', rotation: 0.25 * PI_2, color: 0x00ff00, key: 'ArrowUp' },
+  { order: 3, name: 'right', rotation: 0.5 * PI_2, color: 0x3377ff, key: 'ArrowRight' },
 ];
 
 function getArrowPosition(direction: Direction, arrowWidth: number, appWidth: number): number {
@@ -31,12 +33,19 @@ export class MainScene {
     this.speed = 1.5;
 
     for (const direction of DIRECTIONS) {
+      // Static arrow sprite
       const arrow = Sprite.from('images/arrow.png');
       arrow.anchor.set(0.5);
       arrow.name = direction.name;
       arrow.rotation = direction.rotation;
       arrow.position.set(getArrowPosition(direction, arrow.width, this.width), 60);
       this.container.addChild(arrow);
+
+      // Key handler
+      const key = keyboard(direction.key);
+      key.press = () => {
+        console.log(`${direction.name} pressed`);
+      };
     }
 
     const arrows = new Container();
@@ -53,12 +62,12 @@ export class MainScene {
 
     this.spawnTimer += delta / 60;
     while (this.spawnTimer > 1) {
-      this.spawn(DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)].name);
+      this.spawnArrow(DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)].name);
       this.spawnTimer--;
     }
   }
 
-  spawn(direction: string) {
+  spawnArrow(direction: string) {
     const arrows = this.container.getChildByName('arrows') as Container;
     const arrow = Sprite.from('images/arrow.png');
     arrow.anchor.set(0.5);
