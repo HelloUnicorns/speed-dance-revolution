fdff89e9616a7827759d6423217402c9826f94ae
ClassyLobster <55981867+ClassyLobster@users.noreply.github.com>
2022-10-01 13:14:15 +0300

    Split arrows sprite and load all assets before running.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import { Application } from 'pixi.js';
+import { Assets } from '@pixi/assets';
 import { ArrowsContainer } from './containers/arrows';
 
 const app = new Application({ width: 800, height: 600 });
+app.stop();
+Assets.load(['images/arrow.png']).then(onAssetsLoaded);
 
-const arrowsContainer = ArrowsContainer.create(app.view.width, app.view.height);
-
-app.stage.addChild(arrowsContainer);
+function onAssetsLoaded() {
+  const arrowsContainer = ArrowsContainer.create(app.view.width, app.view.height);
+  app.stage.addChild(arrowsContainer);
+  app.start();
+}
 
 export default app;
