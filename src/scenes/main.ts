a9788f4c002159aa5d95f850a00cb4c6c0215df6
= <ido.weins@gmail.com>
2022-10-02 11:04:07 +0300

    Added feature combo

--- src/scenes/main.ts
+++ src/scenes/main.ts
@@ -7,6 +7,8 @@ const HIT_DISTANCE = 25;
 const ACCELERATION = 1.1;
 const ACCELERATION_TIME_DELTA = 10;
 const HIT_SCORE = 10;
+const MAX_SCORE_COMBO_MULTIPLIER = 11;
+const COMBO_LEVEL_LENGTH = 10;
 
 function getArrowPosition(direction: Direction, arrowWidth: number, appWidth: number): number {
   return appWidth / 2 + (direction.order - 1.5) * arrowWidth * 1.1;
@@ -19,6 +21,7 @@ export class MainScene {
   accelerationTimer: number;
   speed: number;
   score: number;
+  combo: number;
 
   // Graphics
   container: Container;
@@ -30,6 +33,7 @@ export class MainScene {
     this.accelerationTimer = 0;
     this.speed = 1.5;
     this.score = 0;
+    this.combo = 0;
 
     // Graphics
     this.container = new Container();
@@ -44,6 +48,17 @@ export class MainScene {
     scoreLabel.position.set(this.width / 2, this.height);
     scoreLabel.name = 'score';
     this.container.addChild(scoreLabel);
+
+    const comboLabel = new Text('Combo: 0', {
+      fontFamily : 'Arial',
+      fontSize: 32,
+      fill : 0xFFFFFF
+    });
+    comboLabel.anchor.set(0, 1);
+    comboLabel.position.set(0, this.height);
+    comboLabel.name = 'combo';
+    this.container.addChild(comboLabel);
+    this.updateCombo(0);
 
     for (const direction of DIRECTIONS) {
       // Target arrow sprite
@@ -77,15 +92,36 @@ export class MainScene {
     this.container.addChild(arrows);
   }
 
+  updateCombo(newCombo: number) {
+    if (newCombo === this.combo) return;
+    const comboLabel = this.container.getChildByName('combo') as Text;
+    comboLabel.tint = newCombo === 0 ? 0xFF0000 : 0xFFFFFF;
+    comboLabel.text = newCombo === 0 ? "MISS" : "Combo: " + newCombo.toString();
+    this.combo = newCombo;
+  }
+
   hit() {
     console.log('hit');
-    this.score += HIT_SCORE * this.speed;
+
+    this.updateCombo(this.combo + 1);
+    /* The score is multiplied by:
+    *   1 if combo < 10
+    *   2 if 10 <= combo < 20
+    *   3 if 20 <= combo < 30
+    *   ...
+    *   11 if combo >= 100
+    */
+    const comboMultiplier = Math.min(MAX_SCORE_COMBO_MULTIPLIER,
+      1 + Math.floor(this.combo / COMBO_LEVEL_LENGTH));
+    this.score += HIT_SCORE * this.speed * comboMultiplier;
+
     const scoreLabel = this.container.getChildByName('score') as Text;
     scoreLabel.text = "Score: " + Math.round(this.score).toString();
   }
 
   miss(arrow?: ArrowSprite) {
     console.log('miss');
+    this.updateCombo(0);
     if (arrow !== undefined) {
       arrow.missed = true;
     }
