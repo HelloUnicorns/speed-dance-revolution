5635423772fca5c8e2cf3dd947268386eb427f9c
mrnomm <55981867+mrnomm@users.noreply.github.com>
2022-10-03 12:19:12 +0300

    Start funky love.

--- src/songs/funkyLove.ts
+++ src/songs/funkyLove.ts
@@ -3,10 +3,19 @@ import { beatsToSpawnTime, Song } from './song';
 export const funkyLove: Song = beatsToSpawnTime({
   name: 'Funky Love',
   source: 'music/funky_love.mp3',
-  baseArrowSpeed: 1.6,
-  bpm: 106 / 2,
+  baseArrowSpeed: 1.22,
+  bpm: 115,
   fadeOutStart: 140,
   fadeOutEnd: 144,
   end: 144,
-  notes: [],
+  notes: [
+    { time: 0, direction: 'left' },
+    { time: 1, direction: 'right' },
+    { time: 2, direction: 'left' },
+    { time: 3, direction: 'right' },
+    { time: 4, direction: 'left' },
+    { time: 5, direction: 'right' },
+    { time: 6, direction: 'left' },
+    { time: 7, direction: 'right' },
+  ],
 });
